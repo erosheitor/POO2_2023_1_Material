706c2679cbb0b814d21b4e9d040de5141edae96a
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2023-03-22 21:48:32 -0400

    Código encontro 04

--- CSharp/ProjetoPOO2/AtacadoConsole/Program.cs
+++ CSharp/ProjetoPOO2/AtacadoConsole/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AtacadoConsole;
+
+using Atacado.BD.EF.Database;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using AtacadoContext contexto = new AtacadoContext();
+        foreach (Categorium item in contexto.Categoria.ToList())
+        {
+            Console.WriteLine($"{item.Codigo} - {item.Descricao}");
+        }
+        Console.ReadLine();
+    }
+}
